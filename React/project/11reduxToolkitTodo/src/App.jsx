268f4e91748cc00a53b0efe5f7e7dfe61e983920
Sagar Muneshwar <129708548+Sagar1006-dev@users.noreply.github.com>
2024-01-10 18:51:01 +0530

    updateTodo

--- React/project/11reduxToolkitTodo/src/App.jsx
+++ React/project/11reduxToolkitTodo/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 const App = () => {
   return (
     <div>
-      <h1>ReduxToolkit</h1>
-      <AddTodo/>
-      <Todos/>
+      <h1 className="text-4xl font-bold mb-4">ReduxToolkit</h1>
+      <AddTodo />
+      <Todos />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
